5707ecbc09ebd15a3711c6de6c2960d47297cd01
Anand Kumar <97656985+anandroy1@users.noreply.github.com>
2022-03-18 14:45:39 +0530

    Add files via upload

---
+++ React_Project01/src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import "./Card.css";
+import React from "react";
+
+const Card = (props) => {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
